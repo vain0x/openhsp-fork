4d36fe601ea100de8cc1cb471e7f9527d0ffa3da
vain0 <id_vain@yahoo.co.jp>
2015-11-30 17:14:42 +0900

    Commit project files and bat files for development

---
+++ _u_debug.bat
@@ -0,0 +1,3 @@
+copy trunk\hsp3\win32gui\Debug\hsprt.exe           D:\Tool\hsp\hsp3_uedit.exe || pause
+copy trunk\hspcmp\win32dll\Debug\hspcmp3_uedit.dll D:\Tool\hsp\hspcmp_uedit.dll || pause
+copy trunk\hsp3\x64\Debug\hsp3_64.exe              D:\Tool\hsp\hsp3_64_uedit.exe || pause
---
+++ _u_release.bat
@@ -0,0 +1,2 @@
+copy trunk\hsp3\win32gui\Release\hsp3.exe     D:\Tool\hsp\hsp3_uedit.exe || pause
+copy trunk\hspcmp\win32dll\Release\hspcmp.dll D:\Tool\hsp\hspcmp_uedit.dll || pause
